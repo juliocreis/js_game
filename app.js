b25d4cf2a3ffc4e68ee4426e4299fcc3555a4287
Julio Cesar <julioreis951@gmail.com>
2024-05-03 10:59:12 -0300

    melhorando a função verificarChute

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativa = 1;
+console.log(numeroSecreto);
 
 function exibirTextoNaTela (tag, texto){
     let campo = document.querySelector(tag);
@@ -11,6 +13,18 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 function verificarChute() {
     let chute = document.querySelector('input').value
     console.log(chute == numeroSecreto);
+    if(chute == numeroSecreto){
+        let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativa = `Você descobriu o número secreto com ${tentativa} ${palavraTentativa}!`;
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', mensagemTentativa);
+    } else if(chute > numeroSecreto){
+        exibirTextoNaTela('p', 'O número secreto é menor');
+    } else{
+        exibirTextoNaTela('p', 'O número secreto é maior');
+    }
+
+    tentativa++;
 }
 
 function gerarNumeroAleatorio (){
