388db4d5cf463b3ae666be5ab2aea0febc9e65b7
Julio Cesar <julioreis951@gmail.com>
2025-10-07 21:19:00 -0300

    commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function exibirTextoNaTela (tag, texto){
 
 function mensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', `Descubra um númerop entre 1 e ${numeroLimite}`);
+    exibirTextoNaTela('p', `Descubra um número entre 1 e ${numeroLimite}`);
 }
 
 mensagemInicial();
